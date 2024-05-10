81a32f726afb3281f83c479e978f875fcc9738dd
Pascal Garber <pascal@artandcode.studio>
2024-05-10 12:39:49 +0200

    chore(client): Switched to vite again

--- apps/map-editor/vite.config.js
+++ apps/map-editor/vite.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
       },
       external: [new RegExp('^gi://*', 'i'), 'system'],
     },
+    esbuild: {
+      external: ['jsdom'],
+    },
   },
 })
--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,13 +1,44 @@
 import { defineConfig } from 'vite'
+import { createFilter } from '@rollup/pluginutils';
+import glsl from 'vite-plugin-glsl';
+
+function cssAsRaw() {
+  const filter = createFilter('**/*.css');
+
+  return {
+    name: 'css-as-raw',
+    transform(code, id) {
+      if (filter(id)) {
+        return {
+          code: `export default ${JSON.stringify(code)};`,
+          map: { mappings: '' }
+        };
+      }
+    }
+  };
+}
 
 export default defineConfig({
+  plugins: [glsl()],
+  // logLevel: 'info',
   build: {
     outDir: 'dist',
-    lib: {
-      entry: 'index.html',
-      formats: ['esm'],
-      name: 'client',
+    cssCodeSplit: true,
+    publicDir: 'dist',
+    // lib: {
+    //   entry: 'index.html',
+    //   formats: ['esm'],
+    //   name: 'client',
+    // },
+    resolve: {
+      mainFields: ['module', 'main'],
     },
+    sourcemap: true,
+    emptyOutDir: false,
     target: 'esnext',
+    assetsInclude: ['**/*.tmx'],
+    rollupOptions: {
+      plugins: [cssAsRaw()],
+    },
   },
 })
