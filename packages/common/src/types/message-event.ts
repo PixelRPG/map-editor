4225d6294249031f8887d54525477ec0f24071d5
Pascal Garber <pascal@artandcode.studio>
2024-07-01 19:46:57 +0200

    chore(doc): Add some message documentation

--- packages/common/src/types/message-event.ts
+++ packages/common/src/types/message-event.ts
@@ -1,3 +1,7 @@
+/**
+ * Send an Event to the Gtk WebView application or to the WebView client with optional data over postMessage.
+ * For specific Message Event types see the other Interface with the `MessageEvent` prefix.
+ */
 export interface MessageEvent<T = any> {
     type: 'event';
     data: T
--- packages/common/src/types/message-text.ts
+++ packages/common/src/types/message-text.ts
@@ -1,3 +1,7 @@
+/**
+ * Send a text message to the Gtk WebView application or to the WebView client.
+ * Currently only used for debugging.
+ */
 export interface MessageText {
     type: 'text';
     data: string;
