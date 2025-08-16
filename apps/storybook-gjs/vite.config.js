7dc7d91b788eaa0257ef8ae8ccb0f06d22d1a936
Pascal Garber <pascal@artandcode.studio>
2025-08-16 14:30:12 +0200

    chore: Fix lint error warnings

--- apps/storybook-gjs/vite.config.js
+++ apps/storybook-gjs/vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
     // target: "firefox78", // Since GJS 1.65.90
     // target: "firefox91", // Since GJS 1.71.1
     // target: "firefox102", // Since GJS 1.73.2
-    target: "firefox115", // Since GJS 1.77.2
+    // target: "firefox115", // Since GJS 1.77.2
+    target: "firefox128", // Since GJS 1.81.2
     minify: false,
     rollupOptions: {
       input: 'src/main.ts',
